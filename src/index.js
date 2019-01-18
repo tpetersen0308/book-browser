042e49eb684295e9d205ddccbf3686ba271f62bd
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-17 17:10:45 -0800

    move store configuration to store/configureStore.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import registerServiceWorker from './registerServiceWorker';
-import rootReducer from './reducers/rootReducer';
-
-const store = createStore(
-  rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk),
-);
+import store from './store/configureStore';
 
 ReactDOM.render(
   <Provider store={store}>
