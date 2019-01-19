77e226be319300a45d9920720c82e5b4a895d539
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-18 18:15:01 -0800

    build fetchBooks action

--- src/actions/bookActions.js
+++ src/actions/bookActions.js
@@ -1,3 +1,5 @@
+import fetch from 'isomorphic-fetch';
+
 export function addBooks(books) {
   return {
     payload: books,
@@ -5,8 +7,15 @@ export function addBooks(books) {
   }
 }
 
-export function fetchBooks() {
-
+export function fetchBooks(searchTerms) {
+  return dispatch => {
+    dispatch(fetchBooksRequest());
+    return fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerms.join('+')}+intitle&fields=items(volumeInfo/title, volumeInfo/authors, volumeInfo/description)`)
+      .then(data => data.json())
+      .then(json => {
+        dispatch(addBooks(json.items))
+      }).catch(error => console.log(error))
+  }
 }
 
 export function fetchBooksRequest() {
