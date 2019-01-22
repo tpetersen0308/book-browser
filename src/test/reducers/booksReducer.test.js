1e7497fe44f952dc52838fe250bead4d0567a8e7
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-22 11:42:05 -0800

    add test for books reducer

---
+++ src/test/reducers/booksReducer.test.js
@@ -0,0 +1,29 @@
+import booksReducer from '../../reducers/booksReducer';
+
+describe(booksReducer, () => {
+  it('should handle ADD_BOOKS', () => {
+    expect(booksReducer([], {
+      payload: [
+        {
+          title: "Blood Meridian",
+          author: "Cormac McCarthy",
+        },
+        {
+          title: "The Windup Girl",
+          author: "Paolo Bacigalupi",
+        }
+      ], type: "ADD_BOOKS"
+    })).toEqual(
+      [
+        {
+          title: "Blood Meridian",
+          author: "Cormac McCarthy",
+        },
+        {
+          title: "The Windup Girl",
+          author: "Paolo Bacigalupi",
+        }
+      ]
+    )
+  })
+})
