4b572c55a921576f3ec8377f996ecb299be81b51
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-22 17:49:49 -0800

    add page header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Search from './components/Search';
+import { PageHeader } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"></link>
+        <PageHeader>
+          Welcome to BookBrowser!
+          <br />
+          <small>Use the search below to browse Google Books</small>
+        </PageHeader>
         <Search />
       </div>
     );
