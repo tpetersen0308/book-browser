4e980071a7cd8ad40bee0069956c00da71a8174e
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-24 13:18:49 -0800

    add default props and conditional rendering to handle type errors thrown when search results do not contain certain attributes

--- src/components/Book.js
+++ src/components/Book.js
@@ -8,18 +8,28 @@ class Book extends Component {
       <Panel className="book">
         <Panel.Heading className="title">
           <strong>
-            {this.props.title}
+            {this.props.title || "Title Not Available"}
           </strong>
         </Panel.Heading>
         <ListGroup>
           <ListGroupItem className="image"><img src={this.props.imageLink} alt="Image Not Available" /></ListGroupItem>
-          <ListGroupItem className="authors">Author{this.props.authors.length > 1 ? "s" : ""}: {this.props.authors.join(", ")}</ListGroupItem>
+          <ListGroupItem className="authors">Author{this.props.authors.length > 1 ? "s" : ""}: {this.props.authors.length > 0 ? this.props.authors.join(", ") : "Author Not Available"}</ListGroupItem>
           <ListGroupItem className="publisher">Publisher: {this.props.publisher || "Publisher Not Available"}</ListGroupItem>
-          <ListGroupItem className="more-info"><a href={this.props.infoLink} target="_blank" rel="noopener noreferrer">More Info</a></ListGroupItem>
+          {this.props.infoLink &&
+            <ListGroupItem className="more-info"><a href={this.props.infoLink} target="_blank" rel="noopener noreferrer">More Info</a></ListGroupItem>
+          }
         </ListGroup>
       </Panel>
     )
   }
+}
+
+Book.defaultProps = {
+  title: null,
+  authors: [],
+  publisher: null,
+  imageLink: null,
+  infoLink: null,
 }
 
 export default Book;
