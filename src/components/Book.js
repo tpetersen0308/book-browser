1adf7b35727958224cd80f3a4a6cf093ccd258f0
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-24 11:43:03 -0800

    refactor to resolve React console warnings regarding unused component, useless constructor, className attributes and rel attribute in img tag

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
-import { Panel, ListGroup, ListGroupItem, Table } from 'react-bootstrap';
+import { Panel, ListGroup, ListGroupItem } from 'react-bootstrap';
 import './Book.css';
 
 class Book extends Component {
-  constructor(props) {
-    super(props)
-  }
   render() {
     return (
       <Panel className="book">
-        <Panel.Heading class="title">
+        <Panel.Heading className="title">
           <strong>
             {this.props.title}
           </strong>
         </Panel.Heading>
         <ListGroup>
-          <ListGroupItem class="image"><img src={this.props.imageLink} alt="Image Not Available" /></ListGroupItem>
-          <ListGroupItem class="authors">Author{this.props.authors.length > 1 ? "s" : ""}: {this.props.authors.join(", ")}</ListGroupItem>
-          <ListGroupItem class="publisher">Publisher: {this.props.publisher || "Publisher Not Available"}</ListGroupItem>
-          <ListGroupItem class="more-info"><a href={this.props.infoLink} target="_blank">More Info</a></ListGroupItem>
+          <ListGroupItem className="image"><img src={this.props.imageLink} alt="Image Not Available" /></ListGroupItem>
+          <ListGroupItem className="authors">Author{this.props.authors.length > 1 ? "s" : ""}: {this.props.authors.join(", ")}</ListGroupItem>
+          <ListGroupItem className="publisher">Publisher: {this.props.publisher || "Publisher Not Available"}</ListGroupItem>
+          <ListGroupItem className="more-info"><a href={this.props.infoLink} target="_blank" rel="noopener noreferrer">More Info</a></ListGroupItem>
         </ListGroup>
       </Panel>
     )
