2ffae7d6cd584bcd7e6c9be0ff27ad124e7379e5
tpetersen0308 <travis.e.petersen@gmail.com>
2019-01-23 17:22:57 -0800

    add styling to Book

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Panel, ListGroup, ListGroupItem, Table } from 'react-bootstrap';
+import './Book.css';
 
 class Book extends Component {
   constructor(props) {
@@ -7,21 +8,19 @@ class Book extends Component {
   }
   render() {
     return (
-      <div class="book">
-        <Panel>
-          <Panel.Heading class="title">
-            <strong>
-              {this.props.title}
-            </strong>
-          </Panel.Heading>
-          <ListGroup>
-            <ListGroupItem class="image"><img src={this.props.imageLink} alt="Image Not Available" /></ListGroupItem>
-            <ListGroupItem class="authors">Author{this.props.authors.length > 1 ? "s" : ""}: {this.props.authors.join(", ")}</ListGroupItem>
-            <ListGroupItem class="publisher">Publisher: {this.props.publisher || "Publisher Not Available"}</ListGroupItem>
-            <ListGroupItem class="more-info"><a href={this.props.infoLink} target="_blank">More Info</a></ListGroupItem>
-          </ListGroup>
-        </Panel>
-      </div>
+      <Panel className="book">
+        <Panel.Heading class="title">
+          <strong>
+            {this.props.title}
+          </strong>
+        </Panel.Heading>
+        <ListGroup>
+          <ListGroupItem class="image"><img src={this.props.imageLink} alt="Image Not Available" /></ListGroupItem>
+          <ListGroupItem class="authors">Author{this.props.authors.length > 1 ? "s" : ""}: {this.props.authors.join(", ")}</ListGroupItem>
+          <ListGroupItem class="publisher">Publisher: {this.props.publisher || "Publisher Not Available"}</ListGroupItem>
+          <ListGroupItem class="more-info"><a href={this.props.infoLink} target="_blank">More Info</a></ListGroupItem>
+        </ListGroup>
+      </Panel>
     )
   }
 }
