2756a8689318999a16b1540d7b52490725bcc1b7
tpetersen0308 <travis.e.petersen@gmail.com>
2019-02-05 12:35:07 -0800

    refactor state and add case to accommodate addition of fetching status to state in order to have condition to check whether the server has responded

--- src/reducers/booksReducer.js
+++ src/reducers/booksReducer.js
@@ -1,7 +1,12 @@
-function booksReducer(state = [], action) {
+function booksReducer(state = {
+  books: [],
+  fetching: false,
+}, action) {
   switch (action.type) {
     case "ADD_BOOKS":
-      return action.payload;
+      return { books: action.payload, fetching: false };
+    case "FETCH_BOOKS_REQUEST":
+      return { ...state, fetching: true };
     default:
       return state;
   }
